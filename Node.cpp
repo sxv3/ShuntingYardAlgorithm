1f2c3a1d3e04c3b262c661e0f7ddac47116aa17c
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-03-07 00:20:41 -0800

    node class implementation

---
+++ Node.cpp
@@ -0,0 +1,11 @@
+#include "Node.h"
+
+//node class implementation
+//Created by Tai Wong, March 6th
+
+Node::Node(char data) {
+  this->data = data;
+  this->next = NULL;
+  this->left = NULL;
+  this-> right = NULL;
+}
