e014f3cc7b53b567e198bf23e225b0935e3483b7
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-03-06 23:26:10 -0800

    Initial node.h file

---
+++ Node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+//Header file for node class
+//Created by Tai Wong
+
+class Node {
+public:
+  char data;
+  Node* next;
+  Node* left;
+  Node* right;
+
+  Node(char data);
+  
+};
+
+#endif
+  
